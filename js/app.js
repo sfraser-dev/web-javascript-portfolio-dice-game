2b69c00769e88f15c5eec8ee784022dc882c7cf1
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-05-07 20:35:43 +0100

    showing historical result on page correctly now

--- js/app.js
+++ js/app.js
@@ -28,7 +28,41 @@ function playGame(diceImg1, diceImg2, titleElement) {
     let p2Roll = rollTheDice();
     diceImg1.setAttribute("src", `images/dice${p1Roll}.png`);
     diceImg2.setAttribute("src", `images/dice${p2Roll}.png`);
+    // Print who wins this round in the title (h1 heading).
     printOutcomeOfDiceRolls(p1Roll, p2Roll, titleElement);
+    // For each round, store the dice values of each player in JSON format.
+    window.localStorage.setItem(ROLL_COUNTER++, JSON.stringify({ "P1": p1Roll, "P2": p2Roll }));
+}
+
+// Get data from local storage and show it on the page.
+function showHistorialResults() {
+    // Need to delete any previously shown results (or they will just concatenate).
+    deleteHistoricalResults("historicalResults");
+    // Local storage acts like an object which doesn't have order, need to order manually.
+    let orderedArrVals = new Array(window.localStorage.length).fill(0);
+    for (let i = 0; i < window.localStorage.length; i++) {
+        // Get the key/value pairs from local storage. The key is a counter and values are JSON data.
+        const key = window.localStorage.key(i);
+        const valueObj = JSON.parse(window.localStorage.getItem(key));
+        orderedArrVals[key] = valueObj;
+    }
+    // Put ordered historical results onto page.
+    for (let i = 0; i < orderedArrVals.length; i++) {
+        const newParagraph = document.createElement("p");
+        newParagraph.setAttribute("class", "historicalResults");
+        const theOutputText = 
+            `Round ${i+1}, Player1: ${orderedArrVals[i]["P1"]}, Player2: ${orderedArrVals[i]["P2"]}`;
+        newParagraph.innerText = theOutputText;
+        document.body.appendChild(newParagraph);
+    }
+}
+
+// Remove historical results printed to screen as paragraphs.
+function deleteHistoricalResults(className) {
+    const histElements = document.getElementsByClassName(className);
+    while (histElements.length > 0) {
+        histElements[0].parentNode.removeChild(histElements[0]);
+    }
 }
 
 // Get the HTML elements by class name and ID.
@@ -36,17 +70,27 @@ const diceImagePlayer1 = document.getElementsByClassName("img1")[0];
 const diceImagePlayer2 = document.getElementsByClassName("img2")[0];
 const rollBtn = document.getElementsByClassName("btnRoll")[0];
 const resetBtn = document.getElementsByClassName("btnReset")[0];
+const historyBtn = document.getElementsByClassName("btnHistory")[0];
 const theTitleElement = document.getElementById("title");
 const theTitleOriginalText = theTitleElement.innerHTML;
+let ROLL_COUNTER = 0;
 
 // Roll both dice by passing a function with parameters to eventListener via an anonymous function.
-rollBtn.addEventListener("click", function (){playGame(diceImagePlayer1, 
-    diceImagePlayer2, theTitleElement);});
+rollBtn.addEventListener("click", function () {
+    playGame(diceImagePlayer1,
+        diceImagePlayer2, theTitleElement);
+});
 
-// Reset the game by reloading the page.
+// Show dice throw historical results 
+historyBtn.addEventListener("click", showHistorialResults);
+
+// Reset the game by resetting the data and images.
 resetBtn.addEventListener("click", () => {
-    // This arrow function is using global variables which must be declared beforehand.
-    theTitleElement.innerText=theTitleOriginalText;
+    // This arrow function is restting variables which must be declared beforehand.
+    theTitleElement.innerText = theTitleOriginalText;
     diceImagePlayer1.setAttribute("src", "./images/dice6.png");
     diceImagePlayer2.setAttribute("src", "./images/dice6.png");
+    window.localStorage.clear();
+    ROLL_COUNTER = 0;
+    deleteHistoricalResults("historicalResults");
 });
