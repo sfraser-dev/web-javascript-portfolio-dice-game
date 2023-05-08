24bb7540b3d9addffed43d52d63ac348b4aec785
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-05-08 08:45:05 +0100

    use a status object rather than GLOBALS

--- js/app.js
+++ js/app.js
@@ -13,11 +13,11 @@ function rollTheDice() {
 
 // Determine winner of dice roll round and update h1 element with the result.
 function printOutcomeOfDiceRolls(p1, p2, element) {
-    element.innerText = (p1>p2) ? "player 1 wins" : ((p2>p1) ? "player 2 wins" : "it's a draw");
+    element.innerText = (p1 > p2) ? "player 1 wins" : ((p2 > p1) ? "player 2 wins" : "it's a draw");
 }
 
 // Play a single round of the game (roll dice for player 1 and player 2).
-function playGame(diceImg1, diceImg2, titleElement) {
+function playGame(diceImg1, diceImg2, titleElement, statusObj) {
     let p1Roll = rollTheDice();
     let p2Roll = rollTheDice();
     // Update the dice images.
@@ -26,16 +26,17 @@ function playGame(diceImg1, diceImg2, titleElement) {
     // Print who wins this round in the title (h1 heading).
     printOutcomeOfDiceRolls(p1Roll, p2Roll, titleElement);
     // For each round, store the dice values of each player in JSON format.
-    window.localStorage.setItem(ROLL_COUNTER++, JSON.stringify({ "P1": p1Roll, "P2": p2Roll }));
+    window.localStorage.setItem(statusObj["roll counter"]++,
+        JSON.stringify({ "P1": p1Roll, "P2": p2Roll }));
 }
 
 // Get data from local storage and show it on the page.
-function showResultsDatabase() {
+function showResultsDatabase(statusObj) {
     // Alert user if attempting to see updated database without there being any more dice rolls. 
-    if (window.localStorage.length === PREVIOUS_DATABSE_LENGTH) {
+    if (window.localStorage.length === statusObj["previous db length"]) {
         alert("No new dice rolls have been added to the database since the last check.");
     }
-    PREVIOUS_DATABSE_LENGTH = window.localStorage.length;
+    statusObj["previous db length"] = window.localStorage.length;
     // If there is already database info printed on the page, change the button's text.
     if (window.localStorage.length > 0) {
         let showDbBtn = document.getElementsByClassName("btnDatabase")[0];
@@ -55,8 +56,8 @@ function showResultsDatabase() {
     for (let i = 0; i < orderedArrVals.length; i++) {
         const newParagraph = document.createElement("p");
         newParagraph.setAttribute("class", "resultsDatabase");
-        const theOutputText = 
-            `Round ${i+1}, Player1: ${orderedArrVals[i]["P1"]}, Player2: ${orderedArrVals[i]["P2"]}`;
+        const theOutputText =
+            `Round ${i + 1}, Player1: ${orderedArrVals[i]["P1"]}, Player2: ${orderedArrVals[i]["P2"]}`;
         newParagraph.innerText = theOutputText;
         document.body.append(newParagraph);
     }
@@ -78,8 +79,8 @@ function resetEverything() {
     deleteResultsDatabaseFromScreen("resultsDatabase");
     window.localStorage.clear();
     // window.location.reload();
-    ROLL_COUNTER = 0;
-    PREVIOUS_DATABSE_LENGTH = 0;
+    statusObj["roll counter"] = 0;
+    statusObj["previous db length"] = 0;
 }
 
 // Get the HTML elements by class name or ID.
@@ -90,8 +91,7 @@ const resetBtn = document.getElementsByClassName("btnReset")[0];
 const databaseBtn = document.getElementsByClassName("btnDatabase")[0];
 const theTitleElement = document.getElementById("title");
 const theTitleOriginalText = theTitleElement.innerHTML;
-let ROLL_COUNTER = 0;
-let PREVIOUS_DATABSE_LENGTH = 0;
+const statusObj = { "roll counter": 0, "previous db length": 0 };
 
 // Reset text, images and variables to default values. Clear local storage.
 resetEverything();
@@ -99,11 +99,11 @@ resetEverything();
 // Roll both dice by passing a function with parameters to eventListener via an anonymous function.
 rollBtn.addEventListener("click", function () {
     playGame(diceImagePlayer1,
-        diceImagePlayer2, theTitleElement);
+        diceImagePlayer2, theTitleElement, statusObj);
 });
 
 // Button to show dice throw results database.
-databaseBtn.addEventListener("click", showResultsDatabase);
+databaseBtn.addEventListener("click", function () { showResultsDatabase(statusObj); });
 
 // Reset the game by resetting the data and images.
 resetBtn.addEventListener("click", resetEverything);
